36fff363027edf9ebc2ada507be3edba564a374c
dattatray <d.dattatray@gmail.com>
2022-06-25 23:45:06 +0530

    rotate matrix 90 degrees in place formatted

--- src/main/java/arrays/RotateMatrixInPlace.java
+++ src/main/java/arrays/RotateMatrixInPlace.java
@@ -2,28 +2,28 @@ package arrays;
 
 public class RotateMatrixInPlace {
     public static void main(String[] args) {
-        int [][]A = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-            // Transpose the matrix
-            for(int i=0; i<A.length;i++) {
-                for(int j=0; j<i;j++) {
-                    int temp = A[i][j];
-                    A[i][j] = A[j][i];
-                    A[j][i] = temp;
-                }
+        int[][] A = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        // Transpose the matrix
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = A[i][j];
+                A[i][j] = A[j][i];
+                A[j][i] = temp;
             }
-            // Swap the columns
-            for(int i=0; i<A.length; i++) {
-                for(int j=0; j<A.length/2; j++) {
-                    int temp = A[i][j];
-                    A[i][j]= A[i][A.length-j-1];
-                    A[i][A.length-j-1] = temp;
-                }
+        }
+        // Swap the columns
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length / 2; j++) {
+                int temp = A[i][j];
+                A[i][j] = A[i][A.length - j - 1];
+                A[i][A.length - j - 1] = temp;
             }
+        }
         for (int[] ints : A) {
             System.out.println();
             for (int j = 0; j < A.length; j++) {
                 System.out.print(ints[j] + " ");
             }
         }
-        }
+    }
 }
